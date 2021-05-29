6044be3c261854ae0d2a742e08d304099e1fcae1
Jianwei Xie <xiejw0217@gmail.com>
2021-05-28 18:33:28 -0700

    added some sample code to jit.

--- cmd/mnist/main.c
+++ cmd/mnist/main.c
@@ -7,8 +7,58 @@ int
 main()
 {
         printf("hello bb.\n");
-        struct vm_t* vm = bbVmInit();
+        struct vm_t*         vm    = bbVmInit();
+        struct bb_context_t  ctx   = {.vm = vm, .is_training = 1};
+        struct srng64_t*     r     = NULL;
+        struct bb_layer_t*   dense = NULL;
+        struct bb_program_t* p     = NULL;
+        vec_t(int) inputs          = vecNew();
+        vec_t(int) outputs         = vecNew();
 
+        error_t err = bbDenseLayer(
+            vm,
+            &(struct bb_dense_config_t){.input_dim   = 10,
+                                        .output_dim  = 20,
+                                        .kernel_init = BB_INIT_STD_NORMAL,
+                                        .bias_init   = BB_INIT_ZERO,
+                                        .actn        = BB_ACTN_RELU},
+            &dense);
+
+        if (err) {
+                errDump("failed to create dense layer\n");
+                goto cleanup;
+        }
+
+        r = srng64New(123);
+
+        err = dense->init(dense, &ctx, r);
+        if (err) {
+                errDump("failed to init dense layer\n");
+                goto cleanup;
+        }
+
+        struct shape_t* sp = R2S(vm, 32, 10);
+        int             x  = vmTensorNew(vm, F32, sp);
+
+        p = bbProgNew();
+        vecPushBack(inputs, x);
+
+        err = dense->jit(dense, &ctx, p, BB_FORWARD, inputs, &outputs);
+        if (err) {
+                errDump("failed to jit dense layer\n");
+                goto cleanup;
+        }
+
+cleanup:
+
+        vecFree(inputs);
+        vecFree(outputs);
+        if (p) bbProgFree(p);
+        if (r) free(r);
+        if (dense) {
+                dense->release(dense, &ctx);
+                free(dense);
+        };
         vmFree(vm);
         return OK;
 }
