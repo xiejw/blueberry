6c321c2f3bd37ca4386fa6dedb9100cb447f132f
Jianwei Xie <xiejw0217+xiejw2g@gmail.com>
2021-06-01 10:40:37 -0700

    added NE.

--- cmd/mnist/main.c
+++ cmd/mnist/main.c
@@ -4,10 +4,14 @@
 // bb
 #include "bb.h"
 
+// -----------------------------------------------------------------------------
+// Helper Prototype.
+// -----------------------------------------------------------------------------
+
 // Experimental way to create layers.
 //
-// Thoughts: This creates a bunch of layers. It is good, but only works will for
-// seq. For residual network, how to express?
+// Thoughts: This creates a bunch of layers. It is good, but only for sequential
+// layers. For residual network, how to express?
 #define BB_TAG_NULL  0
 #define BB_TAG_DENSE 1
 #define BB_TAG_SCEL  2
@@ -17,7 +21,88 @@ struct bb_layer_config_t {
         void* config;
 };
 
-static error_t
+static error_t bbCreateLayers(struct vm_t*              vm,
+                              struct bb_layer_config_t* layer_configs,
+                              vec_t(struct bb_layer_t*) * layers);
+
+#define NE(err) _NE_IMPL(err, __FILE__, __LINE__)
+
+#define _NE_IMPL(err, file, line)                                          \
+        if ((err)) {                                                       \
+                errDump("unexected error at file %s line %d", file, line); \
+                goto cleanup;                                              \
+        }
+
+// -----------------------------------------------------------------------------
+// Main.
+// -----------------------------------------------------------------------------
+int
+main()
+{
+        printf("hello bb.\n");
+        struct vm_t*        vm           = bbVmInit();
+        struct bb_context_t ctx          = {.is_training = 1};
+        struct srng64_t*    r            = NULL;
+        vec_t(struct bb_layer_t*) layers = vecNew();
+        struct bb_layer_t*   loss_layer  = NULL;
+        struct bb_program_t* p           = NULL;
+        sds_t                s           = sdsEmpty();
+
+        r = srng64New(123);
+        p = bbProgNew();
+
+        struct shape_t* sp = R2S(vm, 32, 10);
+
+        int x = vmTensorNew(vm, F32, sp);
+        int y = vmTensorNew(vm, F32, sp);
+
+        NE(bbCreateLayers(
+            vm,
+            (struct bb_layer_config_t[]){
+                {
+                    .tag = BB_TAG_DENSE,
+                    .config =
+                        &(struct bb_dense_config_t){
+                            .input_dim   = 10,
+                            .output_dim  = 20,
+                            .kernel_init = BB_INIT_STD_NORMAL,
+                            .bias_init   = BB_INIT_ZERO,
+                            .actn        = BB_ACTN_RELU},
+                },
+                {.tag = BB_TAG_SCEL,
+                 .config =
+                     &(struct bb_scel_config_t){.reduction = BB_REDUCTION_SUM}},
+                {.tag = BB_TAG_NULL}},
+            &layers));
+
+        // Take the loss out.
+        loss_layer = layers[vecSize(layers) - 1];
+        vecSetSize(layers, vecSize(layers) - 1);
+
+        NE(bbCompileSeqModule(&ctx, p, x, y, layers, loss_layer, NULL, r));
+
+        bbProgDump(p, &s);
+        printf("%s", s);
+
+cleanup:
+
+        sdsFree(s);
+        for (size_t i = 0; i < vecSize(layers); i++) {
+                bbLayerFree(layers[i]);
+        }
+        bbLayerFree(loss_layer);
+        vecFree(layers);
+        bbProgFree(p);
+        srng64Free(r);
+        vmFree(vm);
+        return OK;
+}
+
+// -----------------------------------------------------------------------------
+// Helper impl.
+// -----------------------------------------------------------------------------
+
+error_t
 bbCreateLayers(struct vm_t* vm, struct bb_layer_config_t* layer_configs,
                vec_t(struct bb_layer_t*) * layers)
 {
@@ -50,74 +135,5 @@ bbCreateLayers(struct vm_t* vm, struct bb_layer_config_t* layer_configs,
                 }
                 curr++;
         }
-        return OK;
-}
-
-int
-main()
-{
-        printf("hello bb.\n");
-        struct vm_t*        vm           = bbVmInit();
-        struct bb_context_t ctx          = {.is_training = 1};
-        struct srng64_t*    r            = NULL;
-        vec_t(struct bb_layer_t*) layers = vecNew();
-        struct bb_layer_t*   loss_layer  = NULL;
-        struct bb_program_t* p           = NULL;
-        sds_t                s           = sdsEmpty();
-
-        r = srng64New(123);
-        p = bbProgNew();
-
-        struct shape_t* sp = R2S(vm, 32, 10);
-
-        int x = vmTensorNew(vm, F32, sp);
-        int y = vmTensorNew(vm, F32, sp);
-
-        error_t err = bbCreateLayers(
-            vm,
-            (struct bb_layer_config_t[]){
-                {
-                    .tag = BB_TAG_DENSE,
-                    .config =
-                        &(struct bb_dense_config_t){
-                            .input_dim   = 10,
-                            .output_dim  = 20,
-                            .kernel_init = BB_INIT_STD_NORMAL,
-                            .bias_init   = BB_INIT_ZERO,
-                            .actn        = BB_ACTN_RELU},
-                },
-                {.tag = BB_TAG_SCEL,
-                 .config =
-                     &(struct bb_scel_config_t){.reduction = BB_REDUCTION_SUM}},
-                {.tag = BB_TAG_NULL}},
-            &layers);
-        if (err) {
-                errDump("failed to create layers\n");
-                goto cleanup;
-        }
-
-        loss_layer = layers[vecSize(layers) - 1];
-        vecSetSize(layers, vecSize(layers) - 1);
-
-        err = bbCompileSeqModule(&ctx, p, x, y, layers, loss_layer, NULL, r);
-        if (err) {
-                errDump("failed to compile module\n");
-                goto cleanup;
-        }
-
-        bbProgDump(p, &s);
-        printf("%s", s);
-
-cleanup:
-
-        sdsFree(s);
-        for (size_t i = 0; i < vecSize(layers); i++) {
-                bbLayerFree(layers[i]);
-        }
-        bbLayerFree(loss_layer);
-        vecFree(layers);
-        bbProgFree(p);
-        srng64Free(r);
-        vmFree(vm);
         return OK;
 }
