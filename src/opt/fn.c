162f112ffbde3f33ae973da0555e70aa99c547c6
Jianwei Xie <xiejw0217@gmail.com>
2021-06-04 14:24:57 -0700

    added map impl.

--- src/opt/fn.c
+++ src/opt/fn.c
@@ -1,5 +1,55 @@
 #include "opt/fn.h"
 
+// a fast map specific for the tensor descriptor. This structure assumes that
+// the input must SSA-like and all tensor descriptor is contiguous.
+struct td_map_t {
+        int cap;
+        vec_t(void*) data;
+};
+
+struct td_map_t*
+bbTdMap(int cap)
+{
+        struct td_map_t* p = malloc(sizeof(struct td_map_t));
+        p->cap             = cap;
+        p->data            = NULL;
+        vecReserve(p->data, cap);
+        memset(p->data, 0, cap * sizeof(void*));  // NULL all slots.
+        return p;
+}
+
+error_t
+bbTdMapFind(struct td_map_t* map, int td, void** data)
+{
+        if (td < 0) return errNew("td cannot be negative.");
+        if (td >= map->cap) return errNew("td is too large.");
+        *data = map->data[td];
+        return OK;
+}
+
+#define BB_TD_MAP_OVERWRITE        0
+#define BB_TD_MAP_DO_NOT_OVERWRITE 1
+
+error_t
+bbTdMapSet(struct td_map_t* map, int td, void* v, int policy, int* existed)
+{
+        if (td < 0) return errNew("td cannot be negative.");
+        if (td >= map->cap) return errNew("td is too large.");
+
+        *existed = map->data[td] != NULL;
+
+        if (policy == BB_TD_MAP_OVERWRITE) {
+                map->data[td] = v;
+                return OK;
+        }
+
+        assert(policy == BB_TD_MAP_DO_NOT_OVERWRITE);
+        if (!*existed) {
+                map->data[td] = v;
+        }
+        return OK;
+}
+
 struct bb_fn_t*
 bbFnNew()
 {
